e078598d48eb057cc486c73bd71c486ffd430ae2
Chris Boyd <chris09boyd@Chriss-MacBook-Pro.local>
2019-02-06 17:08:54 -0800

    C Language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: 1 byte(s)");
 	printf("Size of an int: 4 byte(s)");
 	printf("Size of a long int: 4 byte(s)");
-	prinf("Size of a long long int: 8 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
 	printf("Size of a float: 4 byte(s)");
 	return (0);
 }
