38427702c0cc1ebf8ef405bdcf62484073863f4b
Chris Boyd <chris09boyd@Chriss-MacBook-Pro.local>
2019-02-06 17:48:24 -0800

    C Language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int);
-	       printf("Size of a long int: %ld byte(s)\n", sizeof(int);
-		      printf("Size of a long long int: %lld byte(s)\n", sizeof(int);
-			     printf("Size of a float: %f byte(s)\n", sizeof(int);
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(int));
+	printf("Size of a float: %f byte(s)\n", sizeof(int));
 	return (0);
 }
