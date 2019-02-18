a83d20887c665f8ec3583bf9800574d23658c932
Chris Boyd <chris09boyd@gmail.com>
2019-02-18 04:56:55 +0000

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
