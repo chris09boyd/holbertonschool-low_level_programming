b01e06d9e0af0d67a77914c0d75cbb91020fd89a
Chris Boyd <chris09boyd@gmail.com>
2019-02-19 08:55:18 -0800

    add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+
+/**
+ * int _abs(int) - prints absolute value of an integer
+ * @i: integer to check the vale
+ *
+ * Return: a posive int or 0
+ */
+int _abs(int i)
+{
+	return (i > 0 ? i : -i);
+
+}
