5268bfb1f48e5b5c457b573502c19dddffc0c99f
Chris Boyd <chris09boyd@gmail.com>
2019-02-19 21:08:52 -0800

    add holberton.h

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char b);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _isupper(int c);
+#endif
