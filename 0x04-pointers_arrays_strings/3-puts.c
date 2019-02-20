100a400272bf6725b284030e69ffcedd7d507115
Chris Boyd <chris09boyd@gmail.com>
2019-02-19 20:41:45 -0800

    add 3-puts.c

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* _puts - prints the string with a newline
+* @str: print the string
+*
+*Return: void
+*/
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');	
+}
