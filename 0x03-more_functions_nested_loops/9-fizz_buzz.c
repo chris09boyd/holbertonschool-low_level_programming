f61830db404035e940e4ce3bc90bf9548c86d761
Chris Boyd <chris09boyd@gmail.com>
2019-02-20 20:56:45 -0800

    add 9-fizz_buzz.c

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: Always 0, always success!
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 100; a++)
+	{	
+		if (!(a % 15))
+			printf("FizzBuzz ");
+		else if (!(a % 5))
+			printf("Buzz ");
+		else if (!(a % 3))
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
