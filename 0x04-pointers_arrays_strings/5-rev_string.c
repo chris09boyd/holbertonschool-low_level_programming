bf4eb4443888847ddc4996d2f933d34679e95934
Chris Boyd <chris09boyd@gmail.com>
2019-02-20 18:37:31 -0800

    add 5-rev_string.c

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,13 @@ void rev_string(char *s)
 	int a = 0; b = 0;
 	char c;
 
-	for (a = 0; s[b]; b++)
+	for (b = 0; s[b]; b++)
 			a++;
 
-	for (a = 0; a< 1 / 2; a++)
+	for (b = 0; b < a / 2; b++)
 	{
 		c = s[a - b - 1];
 		s[a - b - 1] = s[b];
-		s[i] = c;
+		s[b] = c;
 	}
-	
+}
