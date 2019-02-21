0bb97da3429add9e3a0cc9b836b2f326d858c4f5
Chris Boyd <chris09boyd@gmail.com>
2019-02-20 19:30:12 -0800

    add 6-puts2.c

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: string necessary to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i, a;
+
+	for (a = 0; str[l]; a++);
+		
+	for (i = 0; i < a; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
