9fb3a45537e320e2c0f331502aada41bb29c988c
Chris Boyd <chris09boyd@gmail.com>
2019-02-20 20:34:41 -0800

    add 5-more_numbers.c

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b / 10)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+
+}
