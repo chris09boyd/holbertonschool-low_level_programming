c062907decb6ba7de8e969266c2105dc444ff5aa
Chris Boyd <chris09boyd@gmail.com>
2019-02-21 21:57:50 -0800

    add 1-strncat.c

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,11 @@
 
 /**
  * _strncat - concatenates two strings
- * @dest: the destination 
+ * @dest: the destination
  * @src: the source
  * @n: amount of bytes that need to be copied
  *
- * Return: pointer to destination 
+ * Return: pointer to destination
  */
 char *_strncat(char *dest, char *src, int n)
 {
