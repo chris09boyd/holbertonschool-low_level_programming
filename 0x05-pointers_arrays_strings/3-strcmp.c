dde70f38a90cd831c7b4b2ce08069d074929c9f1
Chris Boyd <chris09boyd@gmail.com>
2019-02-21 22:05:37 -0800

    add 3-strcmp.c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: '-' if s1 < s2, + if s1 > s2, zero if s1 equals s2
+ * Return: negative if s1 < s2, + if s1 > s2, zero if s1 equals s2
  */
 int _strcmp(char *s1, char *s2)
 {
