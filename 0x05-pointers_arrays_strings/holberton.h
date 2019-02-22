720d0893bf60abb6cda1a04317bae6ecc39f1850
Chris Boyd <chris09boyd@gmail.com>
2019-02-21 22:00:00 -0800

    add holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -31,4 +31,5 @@ void jack_bauer(void);
 void print_square(int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
